b9945eaef7539e7e7e2f872ed420df0ed493345d
Dario Vigil <dariovigil@gmail.com>
2017-11-06 17:53:22 +0100

    Iteration 1.

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,26 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.intervalID = setInterval(() => this.update(), 1000);
+  }
+  stop() {
+    this.clearInterval(this.intervalID);
+  }
+  update() {
+    this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,2 +1,5 @@
-//
+const Person = require ('./person.js');
 const Elevator = require('./elevator.js');
+
+let myElevator = new Elevator;
+myElevator.start();
